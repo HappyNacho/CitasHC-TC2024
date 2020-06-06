b06d2de2a4ef656a66633e2d4c19dd5d184dd2ed
Vargas1723 <vargas23.bv@gmail.com>
2020-06-05 22:28:40 -0500

    Local Again

--- app/src/main/java/com/happynacho/citashc/AgendarFragment.java
+++ app/src/main/java/com/happynacho/citashc/AgendarFragment.java
@@ -61,7 +61,8 @@ public class AgendarFragment extends Fragment {
         btnAgendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ejecutarServicio("https://happynacho.com/insertar_cita.php");
+                ejecutarServicio("http://192.168.100.6:8080/hcg/insertar_cita.php");
+
             }
         });
 
